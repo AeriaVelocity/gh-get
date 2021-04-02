d4e3ae1f2e9117e7f8fbe569aef2f76b556ba555
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-04-02 20:14:33 +0100

    Upload code

---
+++ gh-get.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string gitstring, username, reponame;
+    if (argc >= 4)
+    {
+        username = argv[2];
+        reponame = argv[3];
+
+        if (strcmp(argv[1], "https"))
+        {
+            gitstring = "git clone https://github.com/" + username + "/" + reponame + ".git";
+        }
+        else if (strcmp(argv[1], "ssh"))
+        {
+            gitstring = "git clone git@github.com:" + username + "/" + reponame + ".git";
+        }
+        else
+        {
+            cout << argv[1] << " is invalid - use https or ssh" << endl;
+            exit(1);
+        }
+        system(gitstring.c_str());
+    }
+    else
+    {
+        cout << "Usage: gh-get https|ssh username repo" << endl;
+    }
+    return 0;
+}
