a3d168b29a386fb0406747157fb00bf5d4387a42
Otus9051 <stevewholikesthings@gmail.com>
2021-11-23 22:55:47 +0530

    Add multi site support

--- gh-get.c
+++ gh-get.c
@@ -3,6 +3,8 @@
 gh-get - Makes cloning from a GitHub repo literally as easy as 1, 2, 3. Basically a wrapper for git clone.
 Created by Arsalan Kazmi <That1M8Head>
 
+Edited by Otus9051
+
 This is a C program that was originally written in C++ and manually converted to C.
 
 */
@@ -15,30 +17,30 @@ This is a C program that was originally written in C++ and manually converted to
 int main(int argc, char* argv[])
 {
     /* Variable initialisation */
+    char gitprovider[100] = "";
     char gitstring[140] = "";
     char username[25] = "";
     char reponame[30] = "";
     char destination[40] = "";
 
     /* Checking if the correct amount of arguments is supplied */
-    if (argc >= 3)
+    if (argc >= 4)
     {
-        strcat(username, argv[1]);
-        strcat(reponame, argv[2]);
-        if (argc > 3)
+        strcat(username, argv[2]);
+        strcat(reponame, argv[3]);
+        strcat(gitprovider, argv[1]);
+        if (argc > 4)
         { 
 
             printf("%s will be cloned to ", reponame);
-            strcat(destination, argv[3]);
+            strcat(destination, argv[4]);
             printf("%s\n", destination);
-        }
-        else
-        {
-            printf("No destination provided -- %s will be cloned to new folder in current directory\n", reponame);
         }
 
         /* Form the full git clone string */
-        strcat(gitstring, "git clone https://github.com/");
+        strcat(gitstring, "git clone ");
+        strcat(gitstring, gitprovider);
+        strcat(gitstring, "/");
         strcat(gitstring, username);
         strcat(gitstring, "/");
         strcat(gitstring, reponame);
@@ -46,7 +48,7 @@ int main(int argc, char* argv[])
         strcat(gitstring, destination);
 
         /* Run the git clone command */
-        // printf("%s\n", gitstring); // for debugging
+        /* printf("%s\n", gitstring); */ // for debugging
         system(gitstring);
     }
     else
@@ -54,7 +56,7 @@ int main(int argc, char* argv[])
     {
         /* Inform the user on proper command usage*/
         printf("Incorrect amount of arguments supplied.\n");
-        printf("Usage: gh-get <username> <repo> [destination]\n");
+        printf("Usage: gh-get <gitprovider> <username> <repo> [destination]\n");
     }
     return 0;
 }
